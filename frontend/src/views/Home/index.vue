d41e7345d76f718c3a99bf0e4a011556c2ca7535
Carlos Ribeiro <carlosribeiro@unipam.edu.br>
2020-07-26 12:35:24 -0300

    Improve home page

--- frontend/src/views/Home/index.vue
+++ frontend/src/views/Home/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <v-layout
+    column
+    justify-center
+    align-center
+    class="container"
+  >
+    <h1 class="grey--text">Bem-vindo!</h1>
+    <h5 class="grey--text">Clique em ACESSAR para começar.</h5>
+  </v-layout>
 </template>
 
 <script src="./scripts.js"></script>
+
+<style lang="scss" scoped>
+  @import './styles.scss';
+</style>
