80bac75fe9ec442ce58b8892be9aca0729180f57
Carlos Ribeiro <carlosribeiro@unipam.edu.br>
2020-07-26 12:32:47 -0300

    Improve syntax for APIs

--- frontend/src/api/students.js
+++ frontend/src/api/students.js
@@ -1,11 +1,9 @@
 import { axios } from '@/plugins'
 
-const api = process.env.VUE_APP_API
-
-const getStudents   = ()   => axios.get(`${api}/get-students`)
-const saveStudent   = data => axios.post(`${api}/save-student`, data)
-const editStudent   = data => axios.put(`${api}/edit-student/${data.enrollment_id}`, data)
-const deleteStudent = data => axios.delete(`${api}/delete-student/${data.enrollment_id}`)
+const getStudents   = ()   => axios.get('/get-students')
+const saveStudent   = data => axios.post('/save-student', data)
+const editStudent   = data => axios.put(`/edit-student/${data.enrollment_id}`, data)
+const deleteStudent = data => axios.delete(`/delete-student/${data.enrollment_id}`)
 
 export {
   getStudents,
