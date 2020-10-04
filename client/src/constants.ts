f4985a914c7a3c4551e7730b27dc41e888e34da8
ferco <ferco0@live.com>
2020-10-04 23:42:33 +0000

    Implement student api endpoint configuration
    To work well with ci/cd

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,9 +1,11 @@
-export const APIS_ENDPOINTS = { STUDENTS: "http://192.168.0.105:3000/" };
+export const APIS_ENDPOINTS = {
+  STUDENTS: process.env.VUE_APP_STUDENT_API_ENDPOINT
+};
 export const DARK_THEME_STORAGE_KEY = "disableDark";
 export const STUDENTS_PER_PAGE = 5;
 export const ROUTES = {
   STUDENTS: {
     NAME: "STUDENTS",
-    PATH: "/students",
-  },
+    PATH: "/students"
+  }
 };
