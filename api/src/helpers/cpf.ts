025480672790b76b15697d3838f58c9e2862e58a
Igor Ferreira <igor@pop-os.localdomain>
2020-11-14 18:55:17 -0300

    Api correction allowing to register cpf with repeated numbers

--- api/src/helpers/cpf.ts
+++ api/src/helpers/cpf.ts
@@ -1,28 +1,43 @@
+const cpfWithRepeatedNumbers = [
+  '00000000000',
+  '11111111111',
+  '22222222222',
+  '33333333333',
+  '44444444444',
+  '55555555555',
+  '66666666666',
+  '77777777777',
+  '88888888888',
+  '99999999999',
+];
+
 const checkCpf = (cpf: string): boolean => {
-  let sum: number = 0;
+  let sum = 0;
   let remainder: number;
 
-  if (cpf == '00000000000') return false;
+  if (cpfWithRepeatedNumbers.indexOf(cpf) !== -1) return false;
 
-  for (let i = 1; i <= 9; i++) {
-    sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  for (let i = 1; i <= 9; i += 1) {
+    const multiplication = parseInt(cpf.substring(i - 1, i), 10) * (11 - i);
+    sum += multiplication;
   }
 
   remainder = (sum * 10) % 11;
 
-  if (remainder == 10 || remainder == 11) remainder = 0;
-  if (remainder != parseInt(cpf.substring(9, 10))) return false;
+  if (remainder === 10 || remainder === 11) remainder = 0;
+  if (remainder !== parseInt(cpf.substring(9, 10), 10)) return false;
 
   sum = 0;
-  for (let i = 1; i <= 10; i++) {
-    sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  for (let i = 1; i <= 10; i += 1) {
+    const multiplication = parseInt(cpf.substring(i - 1, i), 10) * (12 - i);
+    sum += multiplication;
   }
 
   remainder = (sum * 10) % 11;
 
-  if (remainder == 10 || remainder == 11) remainder = 0;
+  if (remainder === 10 || remainder === 11) remainder = 0;
 
-  if (remainder != parseInt(cpf.substring(10, 11))) return false;
+  if (remainder !== parseInt(cpf.substring(10, 11), 10)) return false;
   return true;
 };
 
