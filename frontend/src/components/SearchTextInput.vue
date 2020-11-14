76b2b31b6df2c02d4de99680fc9a4c484c924d0c
Igor Ferreira <igor@pop-os.localdomain>
2020-11-14 10:23:18 -0300

    Data table search component

---
+++ frontend/src/components/SearchTextInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-text-field
+    @keyup="handleKeyUp"
+    append-icon="mdi-magnify"
+    type="text"
+    name="input-10-1"
+    label="Digite sua busca"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SearchTextInput',
+  data() {
+    return {
+      debounce: 0,
+    };
+  },
+  methods: {
+    // debounce search
+    handleKeyUp(event: KeyboardEvent) {
+      const text = (event.target as HTMLInputElement).value;
+
+      clearTimeout(this.debounce);
+
+      const timeout = setTimeout(() => {
+        this.$emit('trigger-search', text);
+      }, 1000);
+
+      this.debounce = timeout;
+    },
+  },
+});
+</script>
