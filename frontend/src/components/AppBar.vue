b4b507c9d71d128577bb0849c5061368b96a5016
Igor Ferreira <igor@pop-os.localdomain>
2020-11-14 18:16:23 -0300

    Change title of the theme switch button

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -11,7 +11,7 @@
           <v-icon>mdi-theme-light-dark</v-icon>
         </v-btn>
       </template>
-      <span>Tema Escuro</span>
+      <span>Trocar tema</span>
     </v-tooltip>
   </v-app-bar>
 </template>
