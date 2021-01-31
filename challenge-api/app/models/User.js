d7afa72a84c9682c0a61db95abe8d8d424b14baf
lucas.snta <lucas.snta@gmail.com>
2021-01-31 19:27:44 -0300

    Create User model

---
+++ challenge-api/app/models/User.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const database = require('../config/database/setup')
+
+const User = database.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    username: {
+        type: Sequelize.INTEGER(100),
+        allowNull: false
+    }
+})
