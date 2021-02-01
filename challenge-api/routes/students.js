7f2ce6af9c632e38da2d12ae79aea6cb9b2b1d54
lucas.snta <lucas.snta@gmail.com>
2021-02-01 01:00:11 -0300

    Implement students routes

--- challenge-api/routes/students.js
+++ challenge-api/routes/students.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+//Get controller instance
+const StudentController = require('../app/controllers/StudentController')
+
 //List students
-
-//Create student
-
-//Edit student
-
-//Delete student
+router.get('/', StudentController.list)
 
 //Get Student
+router.get('/:id', StudentController.get)
+
+//Create student
+router.post('/create', StudentController.create)
+
+//Edit student
+router.post('/edit', StudentController.edit)
+
+//Delete student
+router.post('/:id/delete', StudentController.delete)
 
 module.exports = router
