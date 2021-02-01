42bb943299b795a69bfa36f61ac1f2fc6a6d12ff
lucas.snta <lucas.snta@gmail.com>
2021-02-01 02:40:24 -0300

    Implement student controller "crud" functions

--- challenge-api/app/controllers/StudentController.js
+++ challenge-api/app/controllers/StudentController.js
@@ -2,20 +2,103 @@ const Student = require('../models/Student')
 
 //List students
 exports.list = async (req, res) => {
+    let studentList = await Student.findAll()
+    res.json(studentList)
 };
 
 //Get student
 exports.get = async (req, res) => {
+    const id = req.params.id
+    let student = await Student.findByPk(id)
+
+    if (!student) {
+        res.json({'notfound': 'Registro não encontrado'})
+    }
+
+    res.json(student)
 };
 
 //Create student
 exports.create = async (req, res) => {
+    try {
+        let insert = await Student.create({
+            'name': req.body.name,
+            'email': req.body.email,
+            'ra': req.body.ra,
+            'cpf': req.body.cpf
+        })
+
+        res.json({ 
+            'success': { 
+                'id': insert.id,
+                'message': 'Cadastro realizado com sucesso' 
+            }
+        })
+    } catch (err) {
+        res.json({
+            'error': { 
+                'message': err.message
+            }
+        })
+    }
 };
 
 //Edit student
 exports.edit = async (req, res) => {
+    const id = req.body.id
+    let student = await Student.findByPk(id)
+
+    if (!student) {
+        res.json({'notfound': 'Registro não encontrado'})
+    }
+
+    try {
+        await student.update({
+            'name': req.body.name,
+            'email': req.body.email
+        }, 
+        { 
+            where: {
+                'id': id
+            }
+        })
+
+        res.json({ 
+            'success': { 
+                'id': student.id,
+                'message': 'Cadastro atualizado com sucesso' 
+            }
+        })
+    } catch (err) {
+        res.json({
+            'error': { 
+                'message': err.message
+            }
+        })
+    }
 };
 
 //Delete student
 exports.delete = async (req, res) => {
+    const id = req.body.id
+    let student = await Student.findByPk(id)
+
+    if (!student) {
+        res.json({'notfound': 'Registro não encontrado'})
+    }
+
+    try {
+        await student.destroy()
+        res.json({ 
+            'success': { 
+                'message': 'Registro excluído com sucesso' 
+            }
+        })
+    } catch (err) {
+        res.json({
+            'error': { 
+                'message': err.message
+            }
+        })
+    }
 };
