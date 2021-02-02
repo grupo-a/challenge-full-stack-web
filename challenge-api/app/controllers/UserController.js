7c68f678ecc8719bb57d331db9f80fac955bee01
lucas.snta <lucas.snta@gmail.com>
2021-02-01 22:44:59 -0300

    Create User login functions

--- challenge-api/app/controllers/UserController.js
+++ challenge-api/app/controllers/UserController.js
@@ -1,5 +1,33 @@
 const Student = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-//Get student
-exports.get = async (req, res) => {
+//Generate Token
+const createUserToken = (userId) => {
+    return jwt.sign({ id: userId }, 'interstellar', { expiresIn: '1d' })
+}
+
+//Auth User
+exports.auth = async (req, res) => {
+    const { username, password } = req.body
+
+    const student = await Student.findOne({ where: { username: username } })
+
+    if (!student) res.json({ 'error': 'Nome de usuário inválido' })
+
+    bcrypt.compare(password, student.password, (err, same) => {
+        if (!same) {
+            res.json({ 'error': 'Senha incorreta, tente novamente.' })
+        } else {
+            res.json({ 
+                'success': {
+                    'user': {
+                        'name': student.name,
+                        'username': student.username
+                    },
+                    'token': createUserToken(student.id) 
+                } 
+            })
+        }
+    })
 };
