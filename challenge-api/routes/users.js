7864b0b1a0603114a525d9f05720e88ca0ef18fb
lucas.snta <lucas.snta@gmail.com>
2021-02-01 22:46:53 -0300

    Implement user login routes

--- challenge-api/routes/users.js
+++ challenge-api/routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-//Login
+//Call validation middleware
+const { validateAuth } = require('../app/middlewares/validate')
+
+//Get controller instance
+const UserController = require('../app/controllers/UserController')
+
+//Auth
+router.post('/auth', validateAuth, UserController.auth)
 
 module.exports = router
