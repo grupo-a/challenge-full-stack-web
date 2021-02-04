52108b407c10203dc8663ad9fb5e1e6ee64ab585
lucas.snta <lucas.snta@gmail.com>
2021-02-04 14:15:19 -0300

    Implement auth verification on routes

--- challenge-ui/src/router/index.js
+++ challenge-ui/src/router/index.js
@@ -2,24 +2,61 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import List from '../views/List.vue'
 import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Consulta de alunos',
-    component: List
+    name: 'list',
+    component: List,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/cadastro',
-    name: 'Cadastro de Aluno',
-    component: Register
-  }
+    name: 'register',
+    component: Register,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      guest: true
+    }
+  },
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('token') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('token') == null){
+        next()
+      }
+      else{
+        next({ name: 'list'})
+      }
+  }else {
+    next()
+  }
 })
 
 export default router
