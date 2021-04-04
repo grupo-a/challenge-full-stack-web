f2a6f6fa62f46408aac63de8f4c34905204e5831
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-04 19:10:20 -0300

    Add dbConfig with environment variables

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,23 +1,9 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+require('dotenv').config({path: __dirname + '/../../.env'});
+
+module.exports = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dialect: 'postgres'
 }
