58285db30f013f88994f1aa0f2bbd5f630eee222
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-04 17:30:43 -0300

    Add sequelize database connection

---
+++ api/src/config/database.js
@@ -0,0 +1,23 @@
+export default {
+  "development": {
+    "username": "grupoa",
+    "password": "1234",
+    "database": "grupoa",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "grupoa",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "grupoa",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
