7cee7d0778546ca78dd443b62f270a63937557b3
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-04 22:03:13 -0300

    Remove strong-error-handler

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-var errorHandler = require('strong-error-handler');
 
 const StudentsRoutes = require('./routes/StudentsRoutes');
 
@@ -8,10 +7,6 @@ require('./config/database');
 const app = express();
 
 app.use(express.json());
-app.use(errorHandler({
-  debug: process.env.ENV === 'development' || process.env.ENV === 'dev',
-  log: true,
-}));
 app.use(StudentsRoutes);
 
 module.exports = app;
