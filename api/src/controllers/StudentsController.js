d41c48d93b4e9de7f39333a3f7e77b240aeec2c6
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-04 22:09:55 -0300

    Add handling to update, index and show when no users are found

--- api/src/controllers/StudentsController.js
+++ api/src/controllers/StudentsController.js
@@ -9,20 +9,29 @@ const StudentsController = {
   },
   index: async function(req, res) {
     const students = await StudentsRepository.findAll();
-    return res.status(200).json(students);
+    if (students.length > 0){
+      return res.status(200).json(students);
+    }
+    return res.status(404).json({message: "No students found!"});
   },
   show: async function(req, res) {
     const { id } = req.params;
 
     const student = await StudentsRepository.findById(id);
-    return res.status(200).json(student);
+    if(student) {
+      return res.status(200).json(student);
+    }
+    return res.status(404).json({message: `No student found with id=${id}!`});
   },
   update: async function(req, res) {
     const { id } = req.params;
     const { body } = req;
 
     const student = await StudentsRepository.update(id, body);
-    return res.status(200).json(student);
+    if (student) {
+      return res.status(200).json(student);
+    }
+    return res.status(404).json({message: `No student found with id=${id}!`});
   },
   delete: async function (req, res) {
     const { id } = req.params;
