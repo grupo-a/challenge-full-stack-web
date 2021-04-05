a14090edf91f8bf75e6990e2f0c876b45a22e196
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-04 22:32:40 -0300

    Add console logging on error handling

--- api/src/app.js
+++ api/src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(StudentsRoutes);
 app.use((err, req, res, next) => {
+  console.log(err.message);
   res.status(err.status || 500)
   return res.json({
     error: {
