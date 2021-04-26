9f297adb457b74bbcefe1421a0bcefd8d3e215f4
nathielems <nathielems@users.noreply.github.com>
2021-04-26 15:53:48 -0300

    delete unusing files in frontend + adjustments

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" xs="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline mt-5">
          Bem-vindo ao painel de matrículas!
@@ -16,15 +16,3 @@
     </v-col>
   </v-row>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
-</script>
