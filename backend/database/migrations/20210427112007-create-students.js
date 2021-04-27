57d0fb2803c5816a7cf80add6baabb2702dff87b
nathielems <nathielems@users.noreply.github.com>
2021-04-27 09:11:42 -0300

    create sequelize settings, create migration

---
+++ backend/database/migrations/20210427112007-create-students.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('Students',  { 
+        id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      tax: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }, 
+    });
+  },
+};
