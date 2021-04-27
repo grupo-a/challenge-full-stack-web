e8586381c6fc9774b72acb3a5d1b09e21f60cbb5
nathielems <nathielems@users.noreply.github.com>
2021-04-27 15:27:23 -0300

    create sequelize file, students routes & services

--- backend/src/routes/Students.js
+++ backend/src/routes/Students.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/StudentsController')
-router.post('/', controller.post);
-router.put('/:id', controller.put);
+const controller = require('./controllers/StudentsController')
+
+// STUDENTS' ROUTES
+router.insert('/', controller.insert);
+router.getAll('/', controller.getAll);
+router.update('/:id', controller.update);
 router.delete('/:id', controller.delete);
+
+
 module.exports = router;
