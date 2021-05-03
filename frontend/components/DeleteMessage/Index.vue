74010138084e1afb5179208af2c6b86ba12b73f3
nathielems <nathielems@users.noreply.github.com>
2021-05-02 23:30:10 -0300

    adjustment on delete component

--- frontend/components/DeleteMessage/Index.vue
+++ frontend/components/DeleteMessage/Index.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-dialog v-model="dialogDelete" max-width="500px">
+  <v-dialog :value="value" max-width="570px">
     <v-card>
-      <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+      <v-card-title class="headline">Tem certeza de que deseja deletar este registro?</v-card-title>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+        <v-btn color="blue darken-1" text @click="reject">Cancelar</v-btn>
+        <v-btn color="blue darken-1" text @click="accept">Confirmar</v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
+
+<script type="text/javascript" src="./script.js" />
--- frontend/components/DeleteMessage/script.js
+++ frontend/components/DeleteMessage/script.js
@@ -1,83 +1,17 @@
 export default {
   name: 'DeleteMessage',
-  data: () => ({
-    loading: false,
-    items: [],
-    search: null,
-    select: null,
-    dialog: false,
-    dialogDelete: false,
-  }),
-
-  watch: {
-    dialog (val) {
-      val || this.close()
-    },
-    dialogDelete (val) {
-      val || this.closeDelete()
-    },
+  props: {
+    value: {
+        type: Boolean,
+        default: false
+    }
   },
-
-  created () {
-    this.initialize()
-  },
-
   methods: {
-    initialize () {
-      this.desserts = []
+    accept() {
+      this.$emit('accept')
     },
-
-    editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    deleteItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
-
-    deleteItemConfirm () {
-      this.desserts.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
-
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete () {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
-    },
-
-    querySelections (v) {
-      this.loading = true
-      // Simulated ajax query
-      setTimeout(() => {
-        this.items = this.names.filter(e => {
-          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-        })
-        this.loading = false
-      }, 500)
+    reject() {
+      this.$emit('reject')
     },
   },
 }
