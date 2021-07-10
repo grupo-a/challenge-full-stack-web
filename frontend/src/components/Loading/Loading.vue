d1ccb6d79faad71175ee1ede35b94bf67f5e2165
Guilherme <bsbtrindade@gmail.com>
2021-07-10 10:11:58 -0300

    feat: add component loading

---
+++ frontend/src/components/Loading/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-overlay :value="show" color="#21212178" absolute>
+		<v-progress-circular color="primary" indeterminate size="85" style="margin-top: -150px">Aguarde...</v-progress-circular>
+	</v-overlay>
+</template>
+
+<script>
+export default {
+	props: ["show"],
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {},
+};
+</script>
+
+<style lang="sass">
+// .loading-main
+//     position: relative
+
+.v-dialog__content
+    position: absolute
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { ValidationProvider, ValidationObserver } from 'vee-validate';
 import './plugins/vee-validate/extendValidations'
+import Loading from '@/components/Loading/Loading'
 
 Vue.config.productionTip = false
 
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('Loading', Loading)
 
 new Vue({
     router,
