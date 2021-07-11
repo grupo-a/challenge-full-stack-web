47c22a866e4fb4b2969c53a3de338ca5e02487b0
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-11 12:07:27 -0300

    test: ensure Validation Composite  not returns if validation succeeds

--- backend/tests/validation/validators/validation-composite.spec.ts
+++ backend/tests/validation/validators/validation-composite.spec.ts
@@ -42,4 +42,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not returns if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
