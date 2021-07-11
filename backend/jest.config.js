b8306b789ec4b3095e34e87d868a2031fb0b116c
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-11 06:57:06 -0300

    chore: update jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
