f484b95a296a9d3b84f14b2ef1a47d9f3d57aba9
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-11 12:06:54 -0300

    test: ensure Validation Composite returns the first error if more then one validation fails

--- backend/tests/validation/validators/validation-composite.spec.ts
+++ backend/tests/validation/validators/validation-composite.spec.ts
@@ -42,10 +42,4 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
-
-  test('Should not returns if validation succeeds', () => {
-    const { sut } = makeSut()
-    const error = sut.validate({ field: 'any_value' })
-    expect(error).toBeFalsy()
-  })
 })
