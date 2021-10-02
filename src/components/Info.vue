54b3885b1fbc96946d35227f3b44fec000b3fd84
drielisonlopes <drielisonl@gmail.com>
2021-10-02 15:53:48 -0300

    usando diretivas

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
-        <p>Utilizo as seguintes tecnologias:</p>
-        <ul>
-            <li>Javascript</li>
-            <li>Vue</li>
-            <li>NodeJS</li>
+        <p v-if="esta_logado">Aluno está matriculado em algum curso</p>
+        <p v-else>Aluno não matriculado</p>
+        <ul v-show="mostra_dados">
+            <li>Nome</li>
+            <li>Email</li>
+            <li>RA</li>
+            <li>CPF</li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
     export default {
-        name: 'Info'
+        name: 'Info', 
+        data() {
+            return {
+                esta_logado: true,
+                mostra_dados: true
+            }
+        }
     }
 </script>
