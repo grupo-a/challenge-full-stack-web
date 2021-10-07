6858e22e027f9b48da20f60206e093280ded18ff
drielisonlopes <drielisonl@gmail.com>
2021-10-07 15:32:45 -0300

    tornando a API publica

--- backend/routes/cadastros.js
+++ backend/routes/cadastros.js
@@ -13,7 +13,24 @@ router.get('/', (req, res, next) =>{
             'SELECT * FROM cadastros;',
             (error, resultado, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ response: resultado })
+                const response = {
+                    quantidade: resultado.length,
+                    cadastros: resultado.map(cad => {
+                        return {
+                            id_cadastro: cad.id_cadastro,
+                            nome: cad.nome,
+                            email: cad.email,
+                            ra: cad.ra,
+                            cpf: cad.cpf,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna um cadastrado específico',
+                                url: 'http//localhost:3000/cadastros/' + cad.id_cadastro
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response)
             }
         )
     })
@@ -26,15 +43,27 @@ router.post('/', (req, res, next) =>{
         conn.query(
             'INSERT INTO cadastros (nome, email, ra, cpf) VALUES (?,?,?,?)',
             [req.body.nome, req.body.email, req.body.ra, req.body.cpf],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 // release para liberar a conexão do callback
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }                
+                const response = {
+                    mensagem: 'Cadastro realizado com sucesso',
+                        cadastroCriado : {
+                            id_cadastro: result.id_cadastro,
+                            nome: req.body.nome,
+                            email: req.body.email,
+                            ra: req.body.ra,
+                            cpf: req.body.cpf,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna todos os cadastros',
+                                url: 'http//localhost:3000/cadastros'
+                            }
+                    }
+                }
 
-                res.status(201).send({
-                    mensagem: 'Cadastro realizado com sucesso!',
-                    id_cadastro: resultado.insertId
-                })
+                return res.status(201).send(response)
             }
         )
     })
@@ -47,9 +76,29 @@ router.get('/:id_cadastro', (req, res, next) =>{
         if (error) { return res.status(500).send({ error: error }) }
         conn.query (
             'SELECT * FROM cadastros WHERE id_cadastro = ?;',
-            (error, resultado, fields) => {
+            (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ response: resultado })
+
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        mensagem: 'Não foi encontrado cadastro com este ID'
+                    })
+                }
+                const response = {
+                    cadastro : {
+                        id_cadastro: result[0].id_cadastro,
+                        nome: result[0].nome,
+                        email: result[0].email,
+                        ra: result[0].ra,
+                        cpf: result[0].cpf,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Retorna todos os cadastros',
+                            url: 'http//localhost:3000/cadastros'
+                        }
+                    }
+                }
+                return res.status(200).send({ response: result })
             }
         )
     })
@@ -73,13 +122,25 @@ router.patch('/', (req, res, next) =>{
                 req.body.cpf,
                 req.body.id_cadastro
             ],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }  
-
-                res.status(202).send({
-                    mensagem: 'Atualização realizada com sucesso!',
-                })
+                const response = {
+                    mensagem: 'Cadastro atualizado com sucesso',
+                        cadastroAtualizado : {
+                            id_cadastro: req.body.id_cadastro,
+                            nome: req.body.nome,
+                            email: req.body.email,
+                            ra: req.body.ra,
+                            cpf: req.body.cpf,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna detalhes de um cadastro específico',
+                                url: 'http//localhost:3000/cadastros' + req.body.id_produto
+                            }
+                    }
+                }
+                return res.status(202).send(response)
             }
         )
     })
@@ -91,13 +152,24 @@ router.delete('/', (req, res, next) =>{
         if (error) { return res.status(500).send({ error: error }) }
         conn.query(
             `DELETE FROM cadastros WHERE id_cadastro   = ?`, [req.body.id_cadastro],
-            (error, resultado, field) => {
+            (error, result, field) => {
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }  
-
-                res.status(202).send({
+                const response = {
                     mensagem: 'Cadastro excluido com sucesso!',
-                })
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um cadastro',
+                        url: 'http://localhost:3000/cadastros',
+                        body: {
+                            nome: 'String',
+                            email: 'String',
+                            ra: 'Number',
+                            cpf: 'Number'
+                        }
+                    }
+                }
+                return res.status(202).send(response)
             }
         )
     })
