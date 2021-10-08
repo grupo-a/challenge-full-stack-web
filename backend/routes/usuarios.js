3e8898b7909fad38958e49e72bc489be0869e333
drielisonlopes <drielisonl@gmail.com>
2021-10-08 17:07:31 -0300

    organizando rotas c controllers

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,75 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const mysql = require('../mysql').pool
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
-router.post('/registration', (req, res, next) => {
-    mysql.getConnection((err, conn) => {
-        if (err) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'SELECT * FROM usuarios WHERE email = ?',
-            [req.body.email],
-            (error, results) => {
-                if (results.length > 0) {
-                    res.status(409).send({ mensagem: 'Usuário já cadastrado' })
-                } else {
-                    bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-                        if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) }
-                        conn.query(
-                            `INSERT INTO usuarios (email, password) VALUES (?,?)`,
-                            [req.body.email, hash],
-                            (error, results) => {
-                                conn.release();
-                                if (error) { return res.status(500).send({ error: error })}
-                                response = { 
-                                    mensagem: 'Usuário criado com sucesso',
-                                    usuarioCriado: {
-                                        id_usuario: results.insertId,
-                                        email: req.body.email
-                                    }
-                                }
-                                return res.status(201).send(response)
-                            })
-                    })
-                }
-            }
-        )
-    })
-})
+const UsuariosController = require('../controllers/usuaruis-controller')
 
-router.post('/login', (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        const query = `SELECT * FROM usuaruis WHERE email = ?`;
-        conn.query(query, [req.body.email], (error, results, fields) => {
-            conn.release();
-            if (error) { return res.status(500).send({ error: error }) }
-            if (results.length < 1) {
-                return res.status(401).send({ mensagem: 'Falha na autenticação' })
-            }
-            bcryot.compare(req.body.senha, results[0].senha, (err, result) => {
-                if (err) {
-                    return res.status(401).send({ mensagem: 'Falha na autenticação' })
-                }
-                if (result) {
-                    const token = jwt.sign({
-                        id_usuario: results[0].id_usuario,
-                        email: results[0].email
-                    }, 
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    })
-                    return res.status(200).send({
-                        mensagem: 'Autenticado com sucesso',
-                        token: token    
-                    })
-                }
-                return res.status(401).send({ mensagem: 'Falha na autenticação' })
-            })
-        })
-    })
-})
+router.post('/registration', UsuariosController.cadastraUsuario)
+router.post('/login', UsuariosController.Login)
 
 module.exports = router;
