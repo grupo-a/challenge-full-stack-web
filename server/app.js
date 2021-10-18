ad702fab8a4354233e4847bce69154d6f89cbc16
souzskaroline <souzskaroline@hotmail.com>
2021-10-18 20:14:25 -0300

    validation errors with express-validator plugin

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var connection = require('./mysql');
+const { body, validationResult } = require('express-validator');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.get('/students', function (req, res) {
       if (!err)
         res.send(rows)
       else
-        console.log(JSON.stringify(err));
+        res.status(400).json({ msg: 'Erro ao receber dados!' })
     })
 })
 
@@ -35,12 +36,40 @@ app.get('/students/:id', function (req, res) {
       if (!err)
         res.send(rows)
       else
-        console.log(JSON.stringify(err));
+        res.status(400).json({ msg: 'Erro ao receber dados!' })
     })
 })
 
 // INSERT A STUDENT
-app.post('/students', function (req, res) {
+app.post('/students', [
+  // DATA VALIDATION
+  body('email').isEmail().withMessage("E-mail inválido.")
+    .custom(async email => {
+      const value = await isEmailInUse(email);
+      if (value) {
+        throw new Error('E-mail já cadastrado!');
+      }
+    }),
+  body('document').isLength({ min: 11, max: 11 }).withMessage("É necessário ter 11 caracteres.")
+    .custom(async document => {
+      const value = await isDocumentInUse(document);
+      if (value) {
+        throw new Error('Documento já cadastrado!');
+      }
+    }),
+  body('registration').isLength({ min: 9, max: 9 }).withMessage("É necessário ter 9 caracteres.")
+    .custom(async registration => {
+      const value = await isRegistrationInUse(registration);
+      if (value) {
+        throw new Error('RA já cadastrado!');
+      }
+    }),
+], function (req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   connection.query(
     "INSERT INTO student (name, document, registration, email) VALUES (?,?,?,?)",
     [req.body.name, req.body.document, req.body.registration, req.body.email],
@@ -48,21 +77,35 @@ app.post('/students', function (req, res) {
       if (!err)
         res.send({ msg: 'Estudante matriculado com sucesso!' })
       else
-        console.log(JSON.stringify(err));
+        res.status(400).json({ msg: 'Erro ao matricular o estudante!' })
     })
 })
 
-app.put('/students/:id', function (req, res) {
+app.put('/students/:id', [
+  // DATA VALIDATION
+  body('email').isEmail().withMessage("E-mail inválido.")
+    .custom(async email => {
+      const value = await isEmailInUse(email);
+      if (value) {
+        throw new Error('E-mail já cadastrado!');
+      }
+    }),
+], function (req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
   connection.query(
     `UPDATE student 
-    SET name = ?, document = ?, registration = ?, email = ? 
+    SET name = ?, email = ? 
     WHERE id = ?`,
-    [req.body.name, req.body.document, req.body.registration, req.body.email, req.body.id],
+    [req.body.name, req.body.email, req.body.id],
     (err, rows, fields) => {
       if (!err)
         res.send({ msg: 'Estudante atualizado com sucesso!' })
       else
-        console.log(JSON.stringify(err));
+        res.status(400).json({ msg: 'Erro ao atualizar o estudante!' })
     })
 })
 
@@ -73,9 +116,49 @@ app.delete('/students/:id', function (req, res) {
     if (!err)
       res.send({ msg: 'Estudante removido com sucesso.' })
     else
-      console.log(JSON.stringify(err));
+      res.status(400).json({ msg: 'Erro ao remover o estudante!' })
   })
 })
+
+// FUNCTIONS FOR ERROR SUPPORT
+function isDocumentInUse(document) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT COUNT(*) AS total FROM student WHERE document = ?', [document], function (error, results, fields) {
+      if (!error) {
+        return resolve(results[0].total > 0);
+      } else {
+        return reject(new Error('Erro com a conexão.'));
+      }
+    }
+    );
+  });
+}
+
+function isEmailInUse(email) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT COUNT(*) AS total FROM student WHERE email = ?', [email], function (error, results, fields) {
+      if (!error) {
+        return resolve(results[0].total > 0);
+      } else {
+        return reject(new Error('Erro com a conexão.'));
+      }
+    }
+    );
+  });
+}
+
+function isRegistrationInUse(registration) {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT COUNT(*) AS total FROM student WHERE registration = ?', [registration], function (error, results, fields) {
+      if (!error) {
+        return resolve(results[0].total > 0);
+      } else {
+        return reject(new Error('Erro com a conexão.'));
+      }
+    }
+    );
+  });
+}
 
 app.listen(3000);
 
