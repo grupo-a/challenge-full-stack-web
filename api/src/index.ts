925fba09e260018b9944bb8e73173b38008be0de
alexandre-zatti <zatti.alexandre61@gmail.com>
2021-11-27 10:17:27 -0300

    Initial API setup, npm + Typescript + Express + Dockerfile

---
+++ api/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
