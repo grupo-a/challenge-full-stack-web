f2a50af97f59d95c687a6a8b654d05baab780ba0
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-04 09:05:16 -0300

    feat: layout students list

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app
+    dense
+    elevation="4"
+    flat
+    outlined
+  >
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-card color="red darken-1">
+        <v-card-title class="bold white--text">Módulo Acadêmico</v-card-title>
+      </v-card>
+
+      <v-list color="grey lighten-3">
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title>Alunos</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer />
+      <v-text>Consulta de Alunos</v-text>
+      <v-spacer />
+    </v-app-bar>
+    
+    <v-main>
+      <br />
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      drawer: null
+    }
+  },
+};
+</script>
---
+++ frontend/src/components/StudentsList.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="8">
+        <v-text-field 
+          class="pl-10" 
+          outlined
+          autofocus="true"
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Digite sua busca"
+          single-line
+          hide-details
+        />
+      </v-col>
+
+      <v-col class="text-center" cols="4">
+        <v-btn dark x-large color="success">Cadastrar Aluno</v-btn>
+      </v-col>
+    </v-row>
+
+    <br />
+
+    <v-data-table
+      :headers="headers"
+      :items="students"
+      :search="search"
+      class="elevation-1"
+    >
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "StudentsList",
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Registro Acadêmico',
+            align: 'center',
+            sortable: true,
+            value: 'ra',
+          },
+          {
+            text: 'Nome',
+            align: 'center',
+            sortable: true,
+            value: 'name',
+          },
+          {
+            text: 'CPF',
+            align: 'center',
+            sortable: true,
+            value: 'cpf',
+          },
+          {
+            text: 'Ações',
+            align: 'center',
+            sortable: true,
+            value: 'actions',
+          },
+        ],
+        students: [
+          {
+            ra: '1111111',
+            name: 'Lucas',
+            cpf: '111000111100',
+            actions: [
+              '[Editar]',
+              '[Excluir]'
+            ]
+          },
+          {
+            ra: '2222222',
+            name: 'Fulano',
+            cpf: '2222222222',
+            actions: [
+              '[Editar]',
+              '[Excluir]'
+            ]
+          }
+        ],
+      }
+    },
+  }
+</script>
