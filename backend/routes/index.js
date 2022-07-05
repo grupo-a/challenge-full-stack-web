712db1a2cfbee6812fbf2278ccd2c0da93cef43c
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-05 14:15:10 -0300

    add: cors config

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
 const students = require('./studentsRoute');
+const cors = require('cors');
 
 module.exports = app => {
     app
+        .use(cors())
         .use(bodyParser.json())
         .use(students)
 }
