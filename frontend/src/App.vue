ab419bd8364a2d3b0e62dc0cc6fbd50df5c601cb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-05 15:12:55 -0300

    add: home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,35 @@
 <template>
-  <v-app
-    dense
-    elevation="4"
-    flat
-    outlined
-  >
-    <v-navigation-drawer
-      v-model="drawer"
-      app
+  <v-card height="100%">
+    <v-app
+      dense
+      elevation="4"
+      flat
+      outlined
     >
-      <v-card color="red darken-1">
-        <v-card-title class="bold white--text">Módulo Acadêmico</v-card-title>
-      </v-card>
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+      >
+        <v-card color="grey darken-1">
+          <v-card-title class="bold white--text text-center">Módulo Acadêmico</v-card-title>
+        </v-card>
 
-      <v-list color="grey lighten-3">
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>Alunos</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-btn color="grey lighten-2" block to="/">Alunos</v-btn>
+      </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-spacer />
-      <v-text>Consulta de Alunos</v-text>
-      <v-spacer />
-    </v-app-bar>
-    
-    <v-main>
-      <br />
-      <router-view />
-    </v-main>
-  </v-app>
+      <v-app-bar app color="grey lighten-2">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer />
+        <v-toolbar-title>Consulta de Alunos</v-toolbar-title>
+        <v-spacer />
+      </v-app-bar>
+      
+      <v-main>
+        <br />
+        <router-view />
+      </v-main>
+    </v-app>
+  </v-card>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from '../router'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import router from '@/router'
+import store from '@/store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   vuetify,
   render: h => h(App)
