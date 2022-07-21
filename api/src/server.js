19c72adb9949b23de433f1857dda353952c5ac48
Vinicius M Almeida <vinicius.sec21@gmail.com>
2022-07-21 17:04:27 -0300

    enable cors

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors());
 
 const Student = require('./controllers/StudentController');
 
