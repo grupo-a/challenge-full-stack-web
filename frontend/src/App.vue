f33cdc495dad26cd41ada116e9667cee2a11d59e
Vinicius M Almeida <vinicius.sec21@gmail.com>
2022-07-21 22:22:46 -0300

    list students page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,118 @@
 <template>
   <v-app>
+    <v-navigation-drawer app absolute dark src="./assets/livros.jpg" permanent>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar class="white">
+            <v-img src="./assets/logo.png"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>Módulo Acadêmico</v-list-item-title>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-group</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Alunos</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <div>Consulta de alunos</div>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <v-container class="mt-3">
+        <v-row>
+          <v-col>
+            <v-btn color="secondary" class="white--text">Cadastrar Aluno</v-btn>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <v-card-title>
+              Nutrition
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-title>
+            <v-data-table
+              :headers="headers"
+              :items="desserts"
+              :search="search"
+            ></v-data-table>
+          </v-col>
+        </v-row>
+      </v-container>
+      <!-- <HelloWorld /> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+  ra: 'App',
 
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 
   data: () => ({
     //
+    search: '',
+    headers: [
+      {
+        text: 'Registro Acadêmico',
+        value: 'ra',
+      },
+      { text: 'Nome', value: 'name' },
+      { text: 'CPF', value: 'cpf' },
+      { text: 'Ações', value: 'action' },
+    ],
+    desserts: [
+      {
+        ra: 101235,
+        name: 'Paula Souza',
+        cpf: '121.999.999-99',
+        action: '[Editar] [Excluir]',
+      },
+      {
+        ra: 111687,
+        name: 'João Silva',
+        cpf: '122.999.999-99',
+        action: '[Editar] [Excluir]',
+      },
+      {
+        ra: 111365,
+        name: 'Marina Miranda',
+        cpf: '123.999.999-99',
+        action: '[Editar] [Excluir]',
+      },
+      {
+        ra: 101299,
+        name: 'Maurício Souza',
+        cpf: '124.999.999-99',
+        action: '[Editar] [Excluir]',
+      },
+    ],
   }),
 };
 </script>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#173144',
+        secondary: '#C71D37',
+      },
+    },
+  },
 });
