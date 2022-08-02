7b18e5e98d775bb3ae9013c6fd07865a7b7b4dee
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-02 11:52:11 -0300

    Test: Tentando cadastrar passando dados incorreto

--- Back-end/src/Tests/service/service.test.js
+++ Back-end/src/Tests/service/service.test.js
@@ -19,5 +19,42 @@ describe('Service', function () {
       expect(data).to.be.empty;
     });
   });
+
+  describe('#Create', function () {
+    it('Cadastrando um novo estudante', async function () {
+      Sinon.stub(listModel, 'registerStudent').resolves(dataDb[0]);
+      const data = await listService.registerStudent(123, 'Alessandro', '432.322.432-59', 'Alessandro@gmail.com');
+      expect(data).to.deep.eq(dataDb[0]);
+      Sinon.restore();
+    });
+    it('Tentando cadastrar sem ra, retorna a mensagem "ra field is required."', async function () {
+      Sinon.stub(listModel, 'registerStudent').resolves(dataDb[0]);
+      const data = await listService.registerStudent(undefined, 'Alessandro', '432.322.432-59', 'Alessandro@gmail.com');
+
+      expect(data.message).to.deep.eq('ra field is required.');
+      Sinon.restore();
+    });
+    it('Tentando cadastrar sem name, retorna a mensagem "name field is required."', async function () {
+      Sinon.stub(listModel, 'registerStudent').resolves(dataDb[0]);
+      const data = await listService.registerStudent(123, undefined, '432.322.432-59', 'Alessandro@gmail.com');
+
+      expect(data.message).to.deep.eq('name field is required.');
+      Sinon.restore();
+    });
+    it('Tentando cadastrar sem cpf, retorna a mensagem "cpf field is required."', async function () {
+      Sinon.stub(listModel, 'registerStudent').resolves(dataDb[0]);
+      const data = await listService.registerStudent(123, 'Alessandro', undefined, 'Alessandro@gmail.com');
+
+      expect(data.message).to.deep.eq('cpf field is required.');
+      Sinon.restore();
+    });
+    it('Tentando cadastrar sem email, retorna a mensagem "email field is required."', async function () {
+      Sinon.stub(listModel, 'registerStudent').resolves(dataDb[0]);
+      const data = await listService.registerStudent(123, 'Alessandro', '432.322.432-59', undefined);
+
+      expect(data.message).to.deep.eq('email field is required.');
+      Sinon.restore();
+    });
+  });
   
 });
