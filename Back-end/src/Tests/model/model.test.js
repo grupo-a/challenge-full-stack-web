ab87d40cc5b680a7df975ab2cff5ac0f997757a5
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-02 11:06:53 -0300

    Test: Realizando teste de cadastro de estudante

--- Back-end/src/Tests/model/model.test.js
+++ Back-end/src/Tests/model/model.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const { User } = require('../../../models');
-const { dataDb } = require('../Mock');
+const { dataDb, bodyCreate } = require('../Mock');
 const model = require('../../Model');
 
 describe('Models', () => {
@@ -16,6 +16,22 @@ describe('Models', () => {
       sinon.stub(User, 'findAll').resolves([]);
       const data = await model.getAll();
       expect(data).to.be.empty;
+    });
+  });
+
+  describe('#FindOne', () => {
+    afterEach(sinon.restore);
+    it('Cadastrando um estudando no banco', async () => {
+      sinon.stub(User, 'findOne').resolves(false);
+      sinon.stub(User, 'create').resolves(dataDb[0]);
+      const data = await model.registerStudent(123, 'Alessandro', '432.322.432-59', 'Alessandro@gmail.com');
+      expect(data).to.be.equal(dataDb[0]);
+    });
+    it('Se o estudando ja estiver no banco retornar "existing student"', async () => {
+      sinon.stub(User, 'findOne').resolves(true);
+      const data = await model.registerStudent(123, 'Alessandro', '432.322.432-59', 'Alessandro@gmail.com');
+
+      expect(data.message).to.be.equal('existing student');
     });
   });
 
