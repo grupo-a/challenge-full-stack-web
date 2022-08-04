a6c7bd9583b9378f5fb2522e71e7ce480b8daadd
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-04 17:04:03 -0300

    Style: fazendo um estilo no nav

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Lista de Alunos</router-link> |
+  <nav class="container">
+    <router-link to="/" class="students">Lista de Alunos</router-link>
     <router-link to="/cadastrar">
-      <button type="button">Cadastrar</button>
+      <button type="button" class="cadastro">Cadastrar</button>
     </router-link>
   </nav>
   <router-view/>
 </template>
 
-<style>
-
+<style scoped>
+.container {
+  text-align: center;
+}
+.students {
+  margin: 117px;
+  text-decoration: none;
+  font-weight: 900;
+  color: red;
+}
+.cadastro {
+  background-color: rgb(204, 13, 13);
+  border-radius: 5px;
+  margin: 2px;
+  color: white;
+  font-weight: 900;
+  padding: 6px 15px;
+  cursor: pointer;
+}
 </style>
--- front-end/src/components/Students.vue
+++ front-end/src/components/Students.vue
@@ -95,6 +95,7 @@ export default {
   border-radius: 10px;
   margin: 10px;
   padding: 10px;
+  padding-top: 30px;
 }
 
 input {
@@ -117,6 +118,7 @@ input {
   border: none;
   background: none;
   cursor: pointer;
+  padding-top: 20px;
 }
 
 .txt-search {
