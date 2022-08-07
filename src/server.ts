d01aa24780c60e998ce3972436d84e5f58dcf817
CodeAkio <siqueira_2004@hotmail.com>
2022-08-06 21:34:01 -0300

    Fix port number displayed on terminal

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import express from 'express';
 const server = express();
 
 server.listen(3000, () => {
-  console.log('🚀 Server running on port 3333!');
+  console.log('🚀 Server running on port 3000!');
 });
