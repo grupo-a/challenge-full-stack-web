22f8f52ff571d19bdc7dacb221a65f1a02b3b925
riegerjulio <riegerproducoes@gmail.com>
2022-08-24 19:01:11 -0300

    feat: start implementing get route in front-end application

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import router from './routes'
 
 process.on('SIGTERM', () => {
@@ -7,7 +7,15 @@ process.on('SIGTERM', () => {
 
 const app = express();
 
+const accessControl = (req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS, PUT, PATCH');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+
 app.use(express.json());
+app.use(accessControl);
 
 app.use(router);
 
