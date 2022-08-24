24b6e18d5a82d577a863362431bf9c5f0a4be82b
riegerjulio <riegerproducoes@gmail.com>
2022-08-24 16:33:13 -0300

    fix: fixed how sidebar would appear in views

--- front-end/src/views/StudentsView.vue
+++ front-end/src/views/StudentsView.vue
@@ -1,15 +1,20 @@
 <template>
-  <StudentsComponent />
+  <div>
+    <StudentsComponent />
+    <SidebarComponent />
+  </div>
 </template>
 
 <script>
   import StudentsComponent from '../components/StudentsComponent'
+  import SidebarComponent from '../components/SidebarComponent'
 
   export default {
     name: 'Home',
 
     components: {
       StudentsComponent,
+      SidebarComponent
     },
   }
 </script>
