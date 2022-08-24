adc4334c0a6f9f0df0c78421bb396c1f455cbc22
riegerjulio <riegerproducoes@gmail.com>
2022-08-24 17:08:05 -0300

    feat: implementing CRUD feature from vuetify

--- front-end/src/components/StudentsComponent.vue
+++ front-end/src/components/StudentsComponent.vue
@@ -1,54 +1,278 @@
 <template>
-    <v-container
-        class="py-8 px-6"
-        fluid
+  <v-data-table
+    :headers="headers"
+    :items="students"
+    sort-by="ra"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
       >
-        <v-row>
-          <v-col
-            v-for="card in cards"
-            :key="card"
-            cols="12"
-          >
-            <v-card>
-              <v-subheader>{{ card }}</v-subheader>
+        <v-toolbar-title>Registro de Alunos</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Cadastrar Aluno
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
 
-              <v-list two-line>
-                <template v-for="n in 6">
-                  <v-list-item
-
-                    :key="n"
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
                   >
-                    <v-list-item-avatar color="grey darken-1">
-                    </v-list-item-avatar>
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.calories"
+                      label="Calories"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.fat"
+                      label="Fat (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.carbs"
+                      label="Carbs (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.protein"
+                      label="Protein (g)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
 
-                    <v-list-item-content>
-                      <v-list-item-title>Message {{ n }}</v-list-item-title>
-
-                      <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-divider
-                    v-if="n !== 6"
-                    :key="`divider-${n}`"
-                    inset
-                  ></v-divider>
-                </template>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
+
+// <script>
+//   export default {
+//     name: 'StudentsComponent',
+
+//     data: () => ({
+      // headers: [
+      //   { text: 'Registro Acadêmico', align: 'start', sortable: true, value: 'ra' },
+      //   { text: 'Nome', align: 'start', sortable: true, value: 'name' },
+      //   { text: 'CPF', align: 'start', sortable: true, value: 'cpf'  },
+      //   { text: 'Ações', align: 'start', sortable: true, value: 'actions'  },
+      // ],
+//       students: [
+//         { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+//         { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+//         { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+//         { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+//         { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+//       ]
+//     }),
+//   }
+// </script>
 
 <script>
   export default {
-    name: 'StudentsComponent',
-
     data: () => ({
-      cards: ['Consulta de Alunos'],
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        { text: 'Registro Acadêmico', align: 'start', sortable: true, value: 'ra' },
+        { text: 'Nome', align: 'start', sortable: true, value: 'name' },
+        { text: 'CPF', align: 'start', sortable: true, value: 'cpf'  },
+        { text: 'Ações', align: 'start', sortable: true, value: 'actions'  },
+      ],
+      students: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
     }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.students = [
+          { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+          { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+          { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+          { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+          { ra: 'teste1', name: 'nome aleatorio', cpf: '12312312312', actions: 'edit / delete' },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.students.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.students.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.students.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.students[this.editedIndex], this.editedItem)
+        } else {
+          this.students.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
   }
 </script>
