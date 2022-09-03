5e1e4437de26055f39ccb905b3a65c7b074ab858
Jesse Kalil Martins <jessekalyl1491@gmail.com>
2022-09-03 09:59:25 -0300

    Change MeController

--- src/http/controllers/MeController.ts
+++ src/http/controllers/MeController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express"
-import { FindUserService } from "@services/user/FindUserService"
+import { FindOneUserService } from "@services/user/FindOneUserService"
 
 export class MeController {
 	public static async get({ user: { id } }: Request, res: Response) {
 		const {
 			user: { name, email, isAdmin, createdAt },
-		} = await FindUserService.execute(id)
+		} = await FindOneUserService.execute(id)
 
 		return res.json({ me: { name, email, isAdmin, createdAt } })
 	}
