7dfc9228fc910c758c256e6e4944624f9d59f38d
Jesse Kalil Martins <jessekalyl1491@gmail.com>
2022-09-03 10:00:13 -0300

    Implement student controller

--- src/http/controllers/StudentController.ts
+++ src/http/controllers/StudentController.ts
@@ -1,27 +1,36 @@
 import { Request, Response } from "express"
+import { HttpStatusCode } from "@helpers/httpStatusCode"
+import { CreateStudentService } from "@services/student/CreateStudentService"
+import { FindStudentService } from "@services/student/FindStudentService"
+import { UpdateStudentService } from "@services/student/UpdateStudentService"
+import { FindOneStudentService } from "../../services/student/FindOneStudentService"
+import { DeleteStudentService } from "../../services/student/DeleteStudentService"
 
 export class StudentController {
-	public static async list(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
+	public static async find({ query }: Request, res: Response) {
+		const { students, count } = await FindStudentService.execute(query)
+		return res.json({ students, count })
 	}
 
-	public static async find(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
+	public static async findOne({ params }: Request, res: Response) {
+		const { student } = await FindOneStudentService.execute({
+			studentId: params.id,
+		})
+		return res.json({ student })
 	}
 
-	public static async findOne(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
+	public static async create({ body }: Request, res: Response) {
+		await CreateStudentService.execute(body)
+		return res.status(HttpStatusCode.CREATED).send()
 	}
 
-	public static async create(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
+	public static async update({ body, params }: Request, res: Response) {
+		await UpdateStudentService.execute({ ...body, studentId: params.id })
+		return res.status(HttpStatusCode.NO_CONTENT).send()
 	}
 
-	public static async update(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
-	}
-
-	public static async delete(_req: Request, _res: Response) {
-		throw new Error("Not implemented")
+	public static async delete({ params }: Request, res: Response) {
+		await DeleteStudentService.execute({ studentId: params.id })
+		return res.status(HttpStatusCode.NO_CONTENT).send()
 	}
 }
