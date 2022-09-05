7b3dd102b0d874726a8e79064ebd120512c8aff8
Ernane Rezende <ernane.sa@gmail.com>
2022-09-05 17:00:16 -0300

    feat: add student CRUD

---
+++ src/presentation/controllers/AddStudentController.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe'
+import { tokens } from '../../../di/Tokens'
+import { StudentService } from '../../application/service/StudentService'
+import { MissingParamError } from '../errors/MissingParamError'
+import { badRequest, created, serverError } from '../helpers/HttpHelper'
+import { IController } from '../protocols/IController'
+import { HttpRequest, HttpResponse } from '../protocols/IHttp'
+
+@injectable()
+export class AddStudentController implements IController {
+  constructor(
+    @inject(tokens.StudentService)
+    private readonly studentService: StudentService,
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { nome, email, ra, cpf, isActive } = httpRequest.body
+    try {
+      // TODO: Utilizar a lib joy para validar
+      if (!nome || !email || !ra || !cpf || !isActive) {
+        // TODO: Melhorar a verificação
+        return badRequest(new MissingParamError('Todos os dados devem ser preenchidos'))
+      }
+
+      const result = await this.studentService.add({ nome, email, ra, cpf, isActive })
+      return created(result)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
