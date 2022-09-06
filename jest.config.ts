5b8974a9dc9cfc11bf8c1365e3112f59dbf82866
Ernane Rezende <ernane.sa@gmail.com>
2022-09-06 11:56:14 -0300

    fix: change config jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 export default {
+  testEnvironment: 'node',
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: 'ts-jest',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
