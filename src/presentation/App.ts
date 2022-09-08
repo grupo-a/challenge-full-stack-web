e27e4f265c09b427fc992d16a322ba61e2bff04b
Ernane Rezende <ernane.sa@gmail.com>
2022-09-08 09:23:54 -0300

    chore: config swagger

--- src/presentation/App.ts
+++ src/presentation/App.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { Routes } from './http/Routes'
 import { tokens } from '../di/Tokens'
 import { container } from '../di/Container'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from '../docs/swagger.json'
 
 const app = express()
 
@@ -9,5 +11,6 @@ app.use(express.json())
 
 const routes = container.resolve<Routes>(tokens.Routes)
 routes.setupRouter(app)
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 export default app
