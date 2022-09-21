1d72253e926e68cff6b183a1898443eeadea5758
Yan Vidal <yan.vidal@vitta.me>
2022-09-21 07:30:16 -0300

    feat: install and init config to winston for logs

---
+++ src/config/logger.js
@@ -0,0 +1,14 @@
+// logger.js
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.errors({ stack: true }),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }),
+  ],
+});
+
+export default logger;
