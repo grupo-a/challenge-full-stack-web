04691247d6f15049c445456b7a1680e1dcbac031
Yan Vidal <yan.vidal@vitta.me>
2022-09-21 06:56:00 -0300

    feat: config cors and set some properties in express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 dotenv.config();
 
 const port = process.env.PORT || 3330;
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, async () => {
   console.log(`Running on port ${port}`);
