22d6bc90ae687f033273fdb8c9e1ddfccd9f8561
Yan Vidal <yan.vidal@vitta.me>
2022-09-20 22:50:56 -0300

    feat: install and init config to Express and envs with dotenv

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-const initial = () => true;
-initial('foo');
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const port = process.env.PORT || 3330;
+
+const app = express();
+
+app.listen(port, async () => {
+  console.log(`Running on port ${port}`);
+});
