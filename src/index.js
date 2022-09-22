ccbb22ec895684f12c17e8ad33eee04808829df1
Yan Vidal <yan.vidal@vitta.me>
2022-09-22 08:30:05 -0300

    feat: install zod to make validations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-
-dotenv.config()
+//import StudentsService from './services/Students.js'
+//import postgresConnection from './config/database/postgres/postgres.js'
+import router from './routes/index.js'
 
 const port = process.env.PORT || 3330
 
@@ -12,6 +12,16 @@ app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+//const studentsService = new StudentsService(postgresConnection)
+
+//await studentsService.createStudent({
+//  email: 'teste@gmail.com',
+//  ra: '123456',
+//  cpf: '12345678910'
+//})
+
+app.use(router)
 
 app.listen(port, async () => {
   console.log(`Running on port ${port}`)
