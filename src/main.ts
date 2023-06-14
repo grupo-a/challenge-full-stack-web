aa732eb168e55d3f9b9ac0ddfeab29c6ce5e8280
Luis Durante <luisdurante2@outlook.com>
2023-06-14 00:13:03 -0300

    :tada: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
