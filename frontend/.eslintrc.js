c738185c8ca1f6cae0bf46408f6554f4ff3688ab
Rodrigo Barro <rodrigo.correa.barro@gmail.com>
2023-12-19 01:35:48 -0300

    feature: frontend project setup (vite, vue, vuetify, eslint, prettier)

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'prettier'
+    ],
+    rules: {}
+}
