0546e4ec3c1d374907bfcdfa167c29e5bbe13f49
dihgo01 <luka.poke9@gmail.com>
2024-01-31 09:14:02 -0300

    feat(backend): Configured database connection
    add ORM sequelize performed the necessary configurations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [__dirname + '/**/*.entity.ts'],
+    } as SequelizeModule),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ backend/src/database/config.ts
@@ -0,0 +1,26 @@
+module.exports = {
+    dev: {
+        dialect: process.env.DB_DIALECT || 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_DATABASE || 'educational_platform',
+    },
+    test: {
+        dialect: process.env.DB_DIALECT || 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_DATABASE || 'educational_platform',
+    },
+    prod: {
+        dialect: process.env.DB_DIALECT || 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_DATABASE || 'educational_platform',
+    },
+}
