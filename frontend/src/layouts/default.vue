3c8f02892cf6a7fddc9c2791d222448a4bb19c27
dihgo01 <luka.poke9@gmail.com>
2024-02-02 15:22:09 -0300

    started frontend installed vuetify and adjusted the settings

---
+++ frontend/src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <DefaultBar />
+
+    <DefaultView />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import DefaultBar from './default/AppBar.vue'
+  import DefaultView from './default/View.vue'
+</script>
